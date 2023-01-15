eb7b19541f5339c6ba36db16b3e0381d9b217051
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-15 16:00:33 +0400

    in the process

--- src/Solution.java
+++ src/Solution.java
@@ -4,28 +4,18 @@ import java.util.stream.Collectors;
 class Solution {
     public int myAtoi(String s) {
         String number = "0";
-        int res = 0;
 
         String word = s.trim();
         ArrayList<String> digits = new ArrayList<>();
 
         for (int i = 0; i < word.length(); i++) {
-            if (word.length() == 1 && (
-                            word.charAt(0) == '+' ||
-                            word.charAt(0) == '-' ||
-                            word.charAt(0) == '0' ||
-                            word.charAt(0) == '.' ||
-                            word.charAt(0) == ' '
-            )) break;
+            if (word.length() == 1 && !Character.isDigit(word.charAt(0))) break;
 
-            if (word.length() > 1 && (
-                            word.charAt(1) == '+' ||
-                            word.charAt(1) == '-' ||
-                           !Character.isDigit(word.charAt(1)))) break;
+            if (!Character.isDigit(word.charAt(1))) break;
 
             if (Character.isDigit(word.charAt(i)) ||
-                    (word.charAt(0) == '-' && word.length() > 1 && i==0) ||
-                    (word.charAt(0) == '+' && word.length() > 1 && i==0)
+                    (word.charAt(0) == '-' && i==0) ||
+                    (word.charAt(0) == '+' && i==0)
             )
             digits.add(Character.toString(word.charAt(i)));
             else break;
@@ -35,8 +25,8 @@ class Solution {
 
         if (Double.parseDouble(number) < -(Math.pow(2, 31))) return (int) -(Math.pow(2, 31));
         if (Double.parseDouble(number) > Math.pow(2, 31) - 1) return (int) Math.pow(2, 31);
-        res = Integer.parseInt(number);
-        return (int) res;
+        int res = Integer.parseInt(number);
+        return res;
 
     }
 }
