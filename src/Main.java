97cf9e3b1afe4b9a0e615e6a9d6574b2d70cb561
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-15 14:07:23 +0400

    5th save in the process

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         Solution solution = new Solution();
         System.out.println(
                 solution
-                .myAtoi("words and 987"));
+                .myAtoi("+987"));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -3,25 +3,26 @@ import java.util.stream.Collectors;
 
 class Solution {
     public int myAtoi(String s) {
-  //      boolean isNeg;
+        String number;
+        int res = 0;
 
         String word = s.trim();
- //       isNeg = word.charAt(0) == '-';
         ArrayList<String> digits = new ArrayList<>();
 
         for (int i = 0; i < word.length(); i++) {
-            if (Character.isDigit(word.charAt(i)) || word.charAt(i) == '-')
+            if (Character.isDigit(word.charAt(i)) ||
+                    word.charAt(i) == '-' ||
+                    word.charAt(i) == '+')
                 digits.add(Character.toString(word.charAt(i)));
-            else if (digits.isEmpty()) continue;
             else break;
         }
 
-        String number = digits.stream().collect(Collectors.joining(""));
-        int res = Integer.parseInt(number);
+        if (!digits.isEmpty())  {
+            number = digits.stream().collect(Collectors.joining(""));
+            res = Integer.parseInt(number);}
 
         if (res < -(Math.pow(2, 31))) return (int) -(Math.pow(2, 31));
         if (res > Math.pow(2, 31) - 1) return (int) Math.pow(2, 31) - 1;
-    //    if (isNeg) res = Integer.signum((int) res);
         return (int) res;
 
     }
