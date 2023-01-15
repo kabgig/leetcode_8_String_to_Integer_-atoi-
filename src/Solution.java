bccb2fa74b01114835ab8a81afc4690dfb20e02d
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-15 14:52:19 +0400

    6th save in the process

--- src/Solution.java
+++ src/Solution.java
@@ -3,26 +3,26 @@ import java.util.stream.Collectors;
 
 class Solution {
     public int myAtoi(String s) {
-        String number;
+        String number = "0";
         int res = 0;
 
         String word = s.trim();
         ArrayList<String> digits = new ArrayList<>();
 
         for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(1) == '+' || word.charAt(1) == '-') break;
             if (Character.isDigit(word.charAt(i)) ||
                     word.charAt(i) == '-' ||
-                    word.charAt(i) == '+')
-                digits.add(Character.toString(word.charAt(i)));
+                    word.charAt(i) == '+'
+            ) digits.add(Character.toString(word.charAt(i)));
             else break;
         }
 
-        if (!digits.isEmpty())  {
-            number = digits.stream().collect(Collectors.joining(""));
-            res = Integer.parseInt(number);}
+        if (!digits.isEmpty()) number = digits.stream().collect(Collectors.joining(""));
 
-        if (res < -(Math.pow(2, 31))) return (int) -(Math.pow(2, 31));
-        if (res > Math.pow(2, 31) - 1) return (int) Math.pow(2, 31) - 1;
+        if (Double.parseDouble(number) < -(Math.pow(2, 31))) return (int) -(Math.pow(2, 31));
+        if (Double.parseDouble(number) > Math.pow(2, 31) - 1) return (int) Math.pow(2, 31) - 1;
+        res = Integer.parseInt(number);
         return (int) res;
 
     }
