b8040af63da113d292003be898eab2e164bd113f
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-17 15:42:56 +0400

    done solution 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Solution3 solution = new Solution3();
         System.out.println(
-                solution.myAtoi("-91283472332"));
+                solution.myAtoi("  -42"));
     }
 }
--- src/Solution3.java
+++ src/Solution3.java
@@ -5,15 +5,19 @@ public class Solution3 {
     public int myAtoi(String s) {
         if (s.isEmpty()) return 0;
         int sign = 1, i = 0, base = 0;
-        while (s.charAt(i) == ' ') i++;
+        while (i < s.length() && s.charAt(i) == ' ') {
+            i++;
+        }
 
-        if (s.charAt(i) == '+' || s.charAt(i) == '-')
+        if (i < s.length() && (s.charAt(i) == '+' || s.charAt(i) == '-'))
             if (s.charAt(i++) == '-') sign = -1;
 
         while (i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
-            if (base > Math.pow(2, 31))
-                return (sign == 1) ? (int)Math.pow(2, 31) : -(int)Math.pow(2, 31);
-            base = (base * 10) + s.charAt(i++)-'0';
+            if (base > Integer.MAX_VALUE / 10 ||
+                    base == Integer.MAX_VALUE / 10 &&
+                            s.charAt(i) - '0' > 7)
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            base = (base * 10) + s.charAt(i++) - '0';
         }
         return base * sign;
 
